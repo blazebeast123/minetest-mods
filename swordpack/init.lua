46770745a12fb45f5d819a7012722d32a786e13f
blazebeast123 <lord.fire494@gmail.com>
2025-04-10 20:40:02 +0100

    Add files via upload
    the swords deal the same amount of damage if you punch but i will sort that out soon

---
+++ swordpack/init.lua
@@ -0,0 +1,13 @@
+minetest.register_tool('swordpack:tool1',{
+	description = 'adurite sword',
+	inventory_image = 'adurite.png',
+	wield_image = 'adurite.png',
+	wield_scale = {x=2, y=2, z=1}
+})
+
+minetest.register_tool('swordpack:tool2',{
+	description = 'lava sword',
+	inventory_image = 'lava.png',
+	wield_image = 'lava.png',
+	wield_scale = {x=2, y=2, z=1}
+})
