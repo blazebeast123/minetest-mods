d22198b071baa02d2ee2bd7f1888212ef43f1b33
blazebeast123 <lord.fire494@gmail.com>
2025-04-10 20:39:03 +0100

    Add files via upload
    feel free to download

---
+++ quartz/init.lua
@@ -0,0 +1,13 @@
+minetest.register_node('quartz:sample', {
+	description = 'Quartz',
+	tiles = {'quartz.png'},
+	groups = {cracky = 3},
+	wield_scale = {x=1, y=1, z=1},
+})
+
+minetest.register_node('quartz:sample1', {
+	description = 'Rainbow Block',
+	tiles = {'raimbow.png'},
+	groups = {cracky = 3},
+	wield_scale = {x=1, y=1, z=1},
+})
